24ba65fde689b23f203c118890f0c031a05de94e
menglb <lingbinmeng@hotmail.com>
2026-03-23 16:10:12 +0800

    调整整体布局。

--- console/config/defaultSettings.ts
+++ console/config/defaultSettings.ts
@@ -9,19 +9,24 @@ const Settings: ProLayoutProps & {
 } = {
   navTheme: 'light',
   colorPrimary: '#F5222D',
-  layout: 'top',
+  layout: 'mix',
   contentWidth: 'Fluid',
   fixedHeader: true,
   fixSiderbar: true,
   pwa: true,
   logo: "/logo_white.svg",
-  splitMenus: false,
+  splitMenus: true,
   title: '微平台',
   iconfontUrl: '',
+  siderMenuType: 'group',
   token: {
     // 参见ts声明，demo 见文档，通过token 修改样式
     //https://procomponents.ant.design/components/layout#%E9%80%9A%E8%BF%87-token-%E4%BF%AE%E6%94%B9%E6%A0%B7%E5%BC%8F
-    sider: {},
+    sider: {
+      colorMenuBackground: '#fff',
+      colorBgMenuItemSelected: '#fff1f0',
+      colorTextMenuSelected: '#f5222d'
+    },
     header: {
       heightLayoutHeader: 64,
       colorBgHeader: 'rgba(245, 34, 45, 0.8)',
