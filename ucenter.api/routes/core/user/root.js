37ec39050954035ad85f6321a8a40b893ec65036
menglb <lingbinmeng@hotmail.com>
2026-03-27 11:24:34 +0800

    添加更换登录邮箱、手机号的接口。

--- ucenter.api/routes/core/user/root.js
+++ ucenter.api/routes/core/user/root.js
@@ -4,6 +4,8 @@ import {deleteBigField} from '../../../services/core/userUtils.js'
 import {assignUserInfo} from '../../../services/auth/index.js'
 import retSchema from '../../../daos/retSchema.js'
 import {getUserPrivs} from '../../../services/core/userInfo.js'
+import * as smsLoginService from '../../../services/sms/smsLogin.js'
+import * as emailLoginService from '../../../services/email/emailLogin.js'
 
 export default async function (fastify, opts) {
     const userSchema = {$ref: 'fullParamModels#/properties/UserInfo'}
@@ -97,6 +99,56 @@ export default async function (fastify, opts) {
         }
         const userCode = request.userInfo.userCode
         return await userInfoService.updateUserInfo(userCode, request.reqParams, 0)
+    })
+
+    fastify.post('/mobile', {
+        schema: {
+            description: '更换登录手机号',
+            summary: '更换登录手机号',
+            body: {
+                type: 'object',
+                properties: {
+                    mobile: {type: 'string', description: '新手机号'},
+                    smsCode: {type: 'string', description: '短信验证码'}
+                },
+                required: ['mobile', 'smsCode']
+            },
+            tags: ['user'],
+            response: {
+                default: {...getResSwaggerSchema()}
+            }
+        }
+    }, async function (request, reply) {
+        if (!request.userInfo) {
+            return {...retSchema.FAIL_TOKEN_NO_REDIRECT}
+        }
+        const userCode = request.userInfo.userCode
+        return await smsLoginService.updateMobile(request.reqParams, userCode, 0)
+    })
+
+    fastify.post('/email', {
+        schema: {
+            description: '更换登录邮箱',
+            summary: '更换登录邮箱',
+            body: {
+                type: 'object',
+                properties: {
+                    email: {type: 'string', description: '新邮箱'},
+                    emailCode: {type: 'string', description: '邮箱验证码'}
+                },
+                required: ['email', 'emailCode']
+            },
+            tags: ['user'],
+            response: {
+                default: {...getResSwaggerSchema()}
+            }
+        }
+    }, async function (request, reply) {
+        if (!request.userInfo) {
+            return {...retSchema.FAIL_TOKEN_NO_REDIRECT}
+        }
+        const userCode = request.userInfo.userCode
+        return await emailLoginService.updateEmail(request.reqParams, userCode, 0)
     })
 
 }
