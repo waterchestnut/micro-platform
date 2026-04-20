a9d42cb7452fcc86858eba518070f9b9f39bc7d3
menglb <lingbinmeng@hotmail.com>
2026-04-20 08:52:56 +0800

    添加mtm的统计。

--- console/src/pages/Layout.tsx
+++ console/src/pages/Layout.tsx
@@ -3,8 +3,20 @@ import {ConfigProvider, theme} from 'antd'
 import {useRouteProps} from '@umijs/max'
 import {toLogin} from '@/utils/authority'
 import AppsPop from '@/components/AppsPop'
+import {useEffect} from 'react'
 
 export default function Layout() {
+  useEffect(() => {
+    // @ts-ignore
+    const _mtm = window._mtm = window._mtm || []
+    _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'})
+    const d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0]
+    g.async = true
+    g.src = 'https://mtm.jtxuexi.com/js/container_gHsUBGLC.js'
+    // @ts-ignore
+    s.parentNode.insertBefore(g, s)
+  }, [])
+
   const routeProps = useRouteProps()
   const {initialState} = useModel('@@initialState')
   const {currentUser} = initialState || {}
